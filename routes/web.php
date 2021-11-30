31dfac4467ba41d2f978585216e51a21385e4374
Mamba2405 <lepatebakang@gmail.com>
2021-11-30 20:50:36 +0200

    adding login and registration

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/officeAssistant', function () {
     return view('/officeAssistant.dashboard');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
